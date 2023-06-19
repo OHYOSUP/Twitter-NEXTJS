c74df254c5fcba6fa70cede52d50d1d52224ebd0
Yo sup <dhdytjq2@gamil.com>
2023-06-19 11:47:59 +0900

    back cors origin

--- back/app.js
+++ back/app.js
@@ -25,7 +25,7 @@ db.sequelize
   .catch(console.error);
 app.use(
   cors({
-    origin: ["http://localhost:3000", `nodebird.com`, 'http://15.164.103.14'],
+    origin: ["http://localhost:3000", `nodebird.com`, 'http://13.124.246.5'],
     credentials: true,
   })
 );
